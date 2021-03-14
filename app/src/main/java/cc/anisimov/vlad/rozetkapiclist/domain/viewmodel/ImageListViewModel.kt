7c7da79ca76ecaf4936713c798901f34f99429df
Vladyslav <anisimov.vlad@gmail.com>
2021-03-14 22:54:45 +0200

    Add coil; add image list ui v1

--- app/src/main/java/cc/anisimov/vlad/rozetkapiclist/domain/viewmodel/ImageListViewModel.kt
+++ app/src/main/java/cc/anisimov/vlad/rozetkapiclist/domain/viewmodel/ImageListViewModel.kt
@@ -1,20 +1,34 @@
 package cc.anisimov.vlad.rozetkapiclist.domain.viewmodel
 
+import android.content.Context
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import cc.anisimov.vlad.rozetkapiclist.R
+import cc.anisimov.vlad.rozetkapiclist.data.model.RequestResult
 import cc.anisimov.vlad.rozetkapiclist.data.repository.ImageRepo
+import cc.anisimov.vlad.rozetkapiclist.domain.model.Image
 import cc.anisimov.vlad.rozetkapiclist.domain.utility.SingleLiveEvent
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.launch
 
-class ImageListViewModel @ViewModelInject constructor(private val imageRepo: ImageRepo) :
+class ImageListViewModel @ViewModelInject constructor(
+    private val imageRepo: ImageRepo,
+    @ApplicationContext val context: Context
+) :
     ViewModel() {
+    val oImageList = MutableLiveData<List<Image>?>()
     val oError = SingleLiveEvent<String?>()
     val oLoading = MutableLiveData(false)
     fun start() {
         viewModelScope.launch {
             val result = imageRepo.getImageListPage(1)
+            when (result) {
+                is RequestResult.Success -> oImageList.value = result.data
+                is RequestResult.Error -> oError.value = result.exception?.message
+                    ?: context.getString(R.string.unknown_error)
+            }
         }
     }
 }
--- app/src/main/java/cc/anisimov/vlad/rozetkapiclist/ui/fragment/ImageListFragment.kt
+++ app/src/main/java/cc/anisimov/vlad/rozetkapiclist/ui/fragment/ImageListFragment.kt
@@ -4,11 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.VERTICAL
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import cc.anisimov.vlad.rozetkapiclist.R
+import cc.anisimov.vlad.rozetkapiclist.domain.model.Image
 import cc.anisimov.vlad.rozetkapiclist.domain.viewmodel.ImageListViewModel
 import cc.anisimov.vlad.rozetkapiclist.ui.common.BaseFragment
+import coil.load
 import dagger.hilt.android.AndroidEntryPoint
+import eu.davidea.flexibleadapter.FlexibleAdapter
+import eu.davidea.flexibleadapter.items.AbstractFlexibleItem
+import eu.davidea.flexibleadapter.items.IFlexible
+import eu.davidea.viewholders.FlexibleViewHolder
+import kotlinx.android.synthetic.main.fragment_image_list.*
 import kotlinx.android.synthetic.main.loading_overlay.*
 import kotlinx.android.synthetic.main.toolbar.*
 
@@ -30,7 +41,59 @@ class ImageListFragment : BaseFragment() {
         viewModel.start()
         setupErrorHandling(viewModel.oError)
         setupLoading(viewModel.oLoading, loadingOverlay)
+        setupList()
     }
 
+    private fun setupList() {
+        rvImageList.layoutManager = StaggeredGridLayoutManager(2, VERTICAL)
+        val listAdapter = FlexibleAdapter<ImageAdapterItem>(null)
+        rvImageList.adapter = listAdapter
+        viewModel.oImageList.observe(viewLifecycleOwner) { imageList ->
+            if (imageList == null || imageList.isEmpty()) {
+                return@observe
+            }
+            val adapterItems = imageList.map { ImageAdapterItem(it) }
+            listAdapter.addItems(0, adapterItems)
+        }
+    }
 
+    class ImageAdapterItem(private val image: Image) :
+        AbstractFlexibleItem<ImageAdapterItem.PhotoViewHolder>() {
+
+        override fun equals(other: Any?): Boolean {
+            if (other is ImageAdapterItem) {
+                return this.image.id == other.image.id
+            }
+            return false
+        }
+
+        override fun getLayoutRes(): Int {
+            return R.layout.item_image
+        }
+
+        override fun createViewHolder(
+            view: View,
+            adapter: FlexibleAdapter<IFlexible<RecyclerView.ViewHolder>>
+        ): PhotoViewHolder {
+            return PhotoViewHolder(view, adapter)
+        }
+
+        override fun bindViewHolder(
+            adapter: FlexibleAdapter<IFlexible<RecyclerView.ViewHolder>>,
+            holder: PhotoViewHolder,
+            position: Int,
+            payloads: MutableList<Any>?
+        ) {
+            holder.ivThumbnail.load(image.url)
+        }
+
+        override fun hashCode(): Int {
+            return image.id.hashCode()
+        }
+
+        class PhotoViewHolder(view: View, adapter: FlexibleAdapter<*>) :
+            FlexibleViewHolder(view, adapter) {
+            val ivThumbnail: ImageView = view.findViewById(R.id.ivThumbnail)
+        }
+    }
 }
